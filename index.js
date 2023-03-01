2d9a72ac3e0a07816dae45caf3acecef25cef39c
chrisgom113 <cgomez4@uci.edu>
2023-03-01 13:57:13 -0800

    added question form/prompts

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const inquirer = require('inquirer');
 const fs = require('fs');
 
 const generateHTML = () => {
@@ -27,11 +28,46 @@ const generateHTML = () => {
 }
 
 const init = () => {
-    const html = generateHTML();
-    const fileName = 'index.html'
-    fs.writeFile(fileName, html, (err) => {
-        err ? console.log(err) : console.log(`Successfully created ${fileName}!`);
-        })
+    inquirer.prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'what is your name?',
+    },
+    {
+        type: 'input',
+        name: 'location',
+        message: 'Where are you from?',
+    },
+    {
+        type: 'input',
+        name: 'hobby',
+        message: 'What is your favorite hobby?',
+    },
+    {
+        type: 'input',
+        name: 'food',
+        message: 'What is your favorite food?',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username',
+    },
+    {
+        type: 'input',
+        name: 'linkedin',
+        message: 'Enter your LinkedIn URL.',
+    }]).then((answers) => {
+        console.log(answers);
+    });
+
+
+
+    // const html = generateHTML();
+    // const fileName = 'index.html'
+    // fs.writeFile(fileName, html, (err) => {
+    //     err ? console.log(err) : console.log(`Successfully created ${fileName}!`);
+    // })
 }
 
 init();
